bcf943a7bfb0165b1a50afb7d742b1912023eb31
Dean Spirov <spirov92@gmail.com>
2013-02-08 17:01:21 +0200

    Формуляра за лекция да ползва Tk::grid, а не Tk::Table

--- Tk/Lecture.pm
+++ Tk/Lecture.pm
@@ -5,7 +5,6 @@ package Tk::Lecture;
 use base qw/Tk::Frame/;
 use strict;
 use warnings;
-use Tk::Table;
 
 Construct Tk::Widget 'Lecture';
 
@@ -31,22 +30,19 @@ sub submit {
 sub Populate {
 	my ($self, $args) = @_;
 	$self->SUPER::Populate($args);
-	my $table=$self->Table(qw/-rows 5 -columns 2 -scrollbars 0/)->pack(qw/-side top/);
 	
-	my $teacherLabel=$table->Label(qw/-text teacher/);
-	my $teacher=$table->Entry();
-	my $roomLabel=$table->Label(qw/-text room/);
-	my $room=$table->Entry();
-	my $saveBtn=$table->Button('-text'=>'Save', '-command'=> sub { $self->submit(); });
+	my $teacherLabel=$self->Label(qw/-text teacher/);
+	my $teacher=$self->Entry();
+	my $roomLabel=$self->Label(qw/-text room/);
+	my $room=$self->Entry();
+	my $saveBtn=$self->Button('-text'=>'Save', '-command'=> sub { $self->submit(); });
 	$self->{'status'}='';
-	my $statusLabel=$table->Label('-textvariable'=>\$self->{'status'});
+	my $statusLabel=$self->Label('-textvariable'=>\$self->{'status'});
 	
-	$table->put(1,1, $teacherLabel);
-	$table->put(1,2, $teacher);
-	$table->put(2,1, $roomLabel);
-	$table->put(2,2, $room);
-	$table->put(3,1, $saveBtn);
-	$table->put(3,2, $statusLabel);
+	# Arrange widgets in table. See Tk::grid docs.
+	$teacherLabel->grid($teacher);
+	$roomLabel->grid($room);
+	$saveBtn->grid($statusLabel);
 	
 	$self->{'teacher'}=$teacher;
 	$self->{'room'}=$room;
