31c41fb732ffc5b60b3143347f3b1426e372ba9f
Dean Spirov <spirov92@gmail.com>
2013-02-08 16:30:04 +0200

    неподвижен първи ред в таблиците (с имена на колони)

--- Tk/RoomsList.pm
+++ Tk/RoomsList.pm
@@ -19,21 +19,22 @@ sub Refresh {
 	my ($self) = @_;
 	$self->clear;
 	my $rooms=$self->{'schema'}->resultset('Room');
-	my $row=2;
-	$self->put(1,1, "ID");
-	$self->put(1,2, "Name");
-	$self->put(1,3, "Capacity");
+	my $row=1;
+	$self->configure(qw/-fixedrows 1/);
+	$self->put(0,0, "ID");
+	$self->put(0,1, "Name");
+	$self->put(0,2, "Capacity");
 	while(my $room = $rooms->next)
 	{
-		$self->put($row,1, $room->id);
-		$self->put($row,2, $room->name());
-		$self->put($row,3, $room->capacity());
-		$self->put($row,4, $self->Button('-text'=>'view log', '-command'=>sub {
+		$self->put($row,0, $room->id);
+		$self->put($row,1, $room->name());
+		$self->put($row,2, $room->capacity());
+		$self->put($row,3, $self->Button('-text'=>'view log', '-command'=>sub {
 				my $logWindow=$self->Toplevel();
 				$logWindow->configure('-title'=>'Log for '.$room->name);
 				$logWindow->RoomLog()->pack(qw/-side top/)->SetRoom($room);
 			}));
-		$self->put($row,5, $self->Button('-text'=>'start lecture', '-command'=>sub {
+		$self->put($row,4, $self->Button('-text'=>'start lecture', '-command'=>sub {
 				my $startWindow=$self->Toplevel();
 				$startWindow->configure('-title'=>'Start lecture in '.$room->name);
 				my $lecture=$startWindow->LectureStart()->pack(qw/-side top/);
--- Tk/TeacherLog.pm
+++ Tk/TeacherLog.pm
@@ -17,16 +17,17 @@ sub Refresh
 {
 	my ($self) = @_;
 	my $data=$self->{'teacher'}->search_related('lectures');
-	my $row=2;
+	my $row=1;
+	$self->configure(qw/-fixedrows 1/);
 	$self->clear;
-	$self->put(1,1, "Room");
-	$self->put(1,2, "Start");
-	$self->put(1,3, "End");
+	$self->put(0,0, "Room");
+	$self->put(0,1, "Start");
+	$self->put(0,2, "End");
 	while( my $lecture = $data->next )
 	{
-		$self->put($row, 1, $lecture->room->name);
-		$self->put($row, 2, $lecture->start);
-		$self->put($row, 3, $lecture->end) if($lecture->returned);
+		$self->put($row, 0, $lecture->room->name);
+		$self->put($row, 1, $lecture->start);
+		$self->put($row, 2, $lecture->end) if($lecture->returned);
 		$row++;
 	}
 }
